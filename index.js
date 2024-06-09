3e19e1f0e57d9cddf817accb2d0836c947ee905d
imlog4n <realloganwilson1@gmail.com>
2024-06-09 17:34:55 +0000

    .

--- index.js
+++ index.js
@@ -5,34 +5,33 @@ const path = require("path");
 const file = {
     new: (fileName) => {
         fileName = fileName.startsWith("/") ? fileName.substring(1) : fileName;
-        let newRoot = file.root.endsWith("/") || file.root == "" ? file.root : `${file.root}/`;
+        let newRoot = file.root.endsWith("/") || file.root == "" ? `.././${file.root}` : `.././${file.root}/`;
 
         return path.join(__dirname, `${newRoot}${fileName}`);
     },
     root: "",
     this: __filename,
-    router: {
-        port: app.listen,
-        get: (fileName, path) => {
-            app.get(path || "/", (req, res) => {
-                res.sendFile(file.new(fileName));
-            })
-        },
-        post: (fileName, path) => {
-            app.post(path || "/", (req, res) => {
-                res.sendFile(file.new(fileName));
-            })
-        },
-        put: (fileName, path) => {
-            app.put(path || "/", (req, res) => {
-                res.sendFile(file.new(fileName));
-            })
-        },
-        delete: (fileName, path) => {
-            app.delete(path || "/", (req, res) => {
-                res.sendFile(file.new(fileName));
-            })
-        }
+    express,
+    app,
+    get: (fileName, path) => {
+        app.get(path || "/", (req, res) => {
+            res.sendFile(file.new(fileName));
+        })
+    },
+    post: (fileName, path) => {
+        app.post(path || "/", (req, res) => {
+            res.sendFile(file.new(fileName));
+        })
+    },
+    put: (fileName, path) => {
+        app.put(path || "/", (req, res) => {
+            res.sendFile(file.new(fileName));
+        })
+    },
+    delete: (fileName, path) => {
+        app.delete(path || "/", (req, res) => {
+            res.sendFile(file.new(fileName));
+        })
     }
 }
 
